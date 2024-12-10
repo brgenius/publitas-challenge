ae98f113832a40a90b7378e24c3f8d4809b2ada7
Bernardo Bucher <bernardobucher@MacBook-Pro-de-Bernardo.local>
2024-12-10 19:35:57 -0300

    wip: xml batch index builder

--- assignment.rb
+++ assignment.rb
@@ -12,8 +12,8 @@ def is_lower_than_batch_size?(bytes)
   bytes_to_megabytes(bytes) <= 5
 end
 
-def build_batch(lower_batch_index, upper_batch_index)
-  {lower_batch_index: lower_batch_index, upper_batch_index: upper_batch_index}
+def build_batch_indexes(lower_batch_index, upper_batch_index, batch_size)
+  {lower_batch_index: lower_batch_index, upper_batch_index: upper_batch_index, batch_size: batch_size}
 end
 
 
@@ -21,32 +21,42 @@ end
 @itens =  @doc.xpath("//rss//channel//item")
 
 puts "Item count ->"
-item_count = @doc.xpath("count(//rss//channel//item)").to_i
-puts item_count
+@item_count = @doc.xpath("count(//rss//channel//item)").to_i
+puts @item_count
 
-lower_batch_index = 0
-upper_batch_index = item_count
+@batch_queue_indexes = []
 
-batch_queue_indexes = []
-current_batch = build_batch(lower_batch_index, upper_batch_index)
+##
 
-0..upper_batch_index.bsearch do |i|
+def batch_builder
+  lower_batch_index = 0
+  current_batch = build_batch_indexes(lower_batch_index, @item_count, nil)
 
-  puts "Length count ->"
-  
-  batch_search_pattern = "//rss//channel//item[position() >= #{lower_batch_index} and position() <= #{upper_batch_index}]"
-  batch_size = @doc.xpath(batch_search_pattern).to_s.bytesize
-  
-  puts batch_size
-  batch_is_ready? = is_lower_than_batch_size?(batch_size)
+  batch_count = 0 
+  while lower_batch_index < @item_count do
+    search_batch_size_array = lower_batch_index..@item_count
+    search_batch_size_array.bsearch do |i|
+      
+      batch_search_pattern = "//rss//channel//item[position() >= #{lower_batch_index} and position() <= #{i}]"
+      batch_size = @doc.xpath(batch_search_pattern).to_s.bytesize
+      is_batch_ready = is_lower_than_batch_size?(batch_size)
+      
+      puts "Batch from index #{lower_batch_index}-#{i} would be: Size #{bytes_to_megabytes(batch_size)} / #{is_batch_ready}..."
+      
+      if is_batch_ready then
+        @batch_queue_indexes << build_batch_indexes(lower_batch_index, i, batch_size)
+        lower_batch_index = i+1
+      end
+      
+      !is_batch_ready
+    end
 
-  if batch_is_ready? then
-    current_batch.lower_batch_index = lower_batch_index
-    current_batch.lower_batch_index = lower_batch_index
-    
+    batch_count += 1
+    puts "Batch count #{batch_count} >>>>" 
+    p @batch_queue_indexes
   end
-  
-  
+
 end
 
 
+batch_builder
