acd984f5ca38c151ae76f5b8aaf7646c53c55297
Bernardo Bucher <bernardobucher@MacBook-Pro-de-Bernardo.local>
2024-12-10 20:30:49 -0300

    wip: calling external service based on computed batches, synchronously

--- assignment.rb
+++ assignment.rb
@@ -1,8 +1,13 @@
+require 'json'
 require 'nokogiri'
 require 'concurrent'
 require 'concurrent-edge'
 
+require './external_service'
+
 BATCH_SIZE = 5
+
+#util
 
 def bytes_to_megabytes (bytes)
   bytes / (1024.0 * 1024.0) 
@@ -12,39 +17,37 @@ def is_lower_than_batch_size?(bytes)
   bytes_to_megabytes(bytes) <= 5
 end
 
-def build_batch_indexes(lower_batch_index, upper_batch_index, batch_size)
+## factories
+
+def batch_index_factory(lower_batch_index, upper_batch_index, batch_size)
   {lower_batch_index: lower_batch_index, upper_batch_index: upper_batch_index, batch_size: batch_size}
 end
 
+def xml_node_text_extractor(node, prop)
+  item = node.xpath(prop).children.first
+  item.nil? ? "" : item.text
+end
 
-@doc = Nokogiri::XML.parse(File.read("feed.xml"))
-@itens =  @doc.xpath("//rss//channel//item")
+## services
 
-puts "Item count ->"
-@item_count = @doc.xpath("count(//rss//channel//item)").to_i
-puts @item_count
-
-@batch_queue_indexes = []
-
-##
-
-def batch_builder
+def batch_index_builder(xml_feed, item_count)
+  batch_indexes_queue = []
   lower_batch_index = 0
-  current_batch = build_batch_indexes(lower_batch_index, @item_count, nil)
+  current_batch = batch_index_factory(lower_batch_index, item_count, nil)
 
   batch_count = 0 
-  while lower_batch_index < @item_count do
-    search_batch_size_array = lower_batch_index..@item_count
+  while lower_batch_index < item_count do
+    search_batch_size_array = lower_batch_index..item_count
     search_batch_size_array.bsearch do |i|
       
       batch_search_pattern = "//rss//channel//item[position() >= #{lower_batch_index} and position() <= #{i}]"
-      batch_size = @doc.xpath(batch_search_pattern).to_s.bytesize
+      batch_size = xml_feed.xpath(batch_search_pattern).to_s.bytesize
       is_batch_ready = is_lower_than_batch_size?(batch_size)
       
-      puts "Batch from index #{lower_batch_index}-#{i} would be: Size #{bytes_to_megabytes(batch_size)} / #{is_batch_ready}..."
-      
       if is_batch_ready then
-        @batch_queue_indexes << build_batch_indexes(lower_batch_index, i, batch_size)
+        # puts "Batch from index #{lower_batch_index}-#{i} will be: Size #{bytes_to_megabytes(batch_size)} / #{is_batch_ready}..."
+      
+        batch_indexes_queue << batch_index_factory(lower_batch_index, i, batch_size)
         lower_batch_index = i+1
       end
       
@@ -53,10 +56,80 @@ def batch_builder
 
     batch_count += 1
     puts "Batch count #{batch_count} >>>>" 
-    p @batch_queue_indexes
   end
+  # p batch_indexes_queue
 
+  batch_indexes_queue
 end
 
+def batch_builder(xml_feed, batch_index)
+  # p batch_index
+  batch_search_pattern = "//rss//channel//item[position() >= #{batch_index[:lower_batch_index]} and position() <= #{batch_index[:upper_batch_index]}]"
+  current_batch_itens = xml_feed.xpath(batch_search_pattern).flat_map do |item|
+    {
+      description: xml_node_text_extractor(item, "description"),
+      title: xml_node_text_extractor(item, "title"),
+      id: xml_node_text_extractor(item, "g:id").to_s
+    }
+  end 
+end
 
-batch_builder
+def batch_sender(external_service, xml_feed, batch_queue)
+  batch_queue.each do |current_batch_itens_indexes|
+    
+    json_out  = JSON.generate(batch_builder(xml_feed, current_batch_itens_indexes))
+    # p json_out
+    external_service.call( json_out )
+  end
+end
+
+#main vars
+
+xml_feed = Nokogiri::XML.parse(File.read("feed.xml"))
+# @itens =  @doc.xpath("//rss//channel//item")
+
+puts "Item count ->"
+item_count = xml_feed.xpath("count(//rss//channel//item)").to_i
+puts item_count
+
+batch_indexes_queue = batch_index_builder(xml_feed, item_count)
+
+external_service = ExternalService.new
+batch_sender(external_service, xml_feed, batch_indexes_queue)
+
+
+
+# messages = Concurrent::Channel.new( capacity: 10 )
+
+# Concurrent::Channel.go do
+
+#   p @itens.count 
+#   @itens.each_with_index do |item, i|
+#     current_item = {
+#       description: item.xpath("description").children.first.text,
+#       title: item.xpath("title").children.first.text,
+#       id: item.xpath("g:id").children.first.to_s
+#     }  
+#     parsed_item = {
+#       item: current_item, 
+#       size: current_item.to_s.bytesize,
+#       index: i 
+#     }
+#     puts "Concurrent"
+#     p parsed_item
+
+#     messages << parsed_item
+#   end
+# end
+
+# 
+
+# Concurrent::Channel.select do |s|
+  #s.take(c1) { |msg| print "received #{msg}\n" }
+  #s.take(c2) { |msg| print "received #{msg}\n" }
+
+  # msg = ~messages
+  # puts msg
+  # s.take(messages) { |msg| print "received #{msg}\n" }
+# end
+
