3111049ea0b34510c8249b972a8ba08c58c24ff7
Bernardo Bucher <bernardobucher@MacBook-Pro-de-Bernardo.local>
2024-12-10 21:33:13 -0300

    adds comments

--- assignment.rb
+++ assignment.rb
@@ -31,6 +31,15 @@ end
 
 ## services
 
+# What it does:
+#  Searches an index between the current start index ( lower_batch_index ) and the total item count
+#  that has a lower 5MB total string size in bytes, including all item attributes.
+#  The goal here is to find as quick as possible, item indexes from feed that later will be sent to ExternalService
+# 
+# Why it was done this way:
+#  As binary search based on indexes, some batches will not be EXACTLY arround 5mb,
+#  but it's faster than do a sequential search testing each index, formming perfect 5mb batches.
+#  It also relies on Nokogiri gem ability to search inside the xml file by xpath queries 
 def batch_index_search(xml_feed, lower_batch_index, item_count)
   (lower_batch_index..item_count).to_a.bsearch_index do |i|
     
@@ -38,8 +47,8 @@ def batch_index_search(xml_feed, lower_batch_index, item_count)
     batch_size = xml_feed.xpath(batch_search_pattern).to_s.bytesize
     is_batch_ready = is_lower_than_batch_size?(batch_size)
     
-    puts "Batch from index #{lower_batch_index}-#{i} will be: Size #{bytes_to_megabytes(batch_size)} / #{is_batch_ready}..."
     if is_batch_ready then
+      puts "Batch from index #{lower_batch_index}-#{i} will be: Size #{bytes_to_megabytes(batch_size)} / #{is_batch_ready}..."
       return {
         batch_index: batch_index_factory(lower_batch_index, i, batch_size),
         new_lower_batch_index: i
@@ -50,6 +59,7 @@ def batch_index_search(xml_feed, lower_batch_index, item_count)
   end
 end
 
+# Generates a queue of batch indexes to be extracted later and sent to ExternalService
 def batch_index_builder(xml_feed, item_count)
   batch_indexes_queue = []
   lower_batch_index = 0
@@ -70,7 +80,6 @@ def batch_index_builder(xml_feed, item_count)
 end
 
 def batch_builder(xml_feed, batch_index)
-  # p batch_index
   batch_search_pattern = "//rss//channel//item[position() >= #{batch_index[:lower_batch_index]} and position() <= #{batch_index[:upper_batch_index]}]"
   current_batch_itens = xml_feed.xpath(batch_search_pattern).flat_map do |item|
     {
