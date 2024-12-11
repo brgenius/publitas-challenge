88cd9ff6ab7aa74fc22e61be701bdceec27eb0fd
Bernardo Bucher <bernardobucher@MacBook-Pro-de-Bernardo.local>
2024-12-10 23:00:19 -0300

    refactor

--- assignment.rb
+++ assignment.rb
@@ -2,8 +2,7 @@ require './requirements'
 
 feed = Infra::Feed.get_from_file("feed.xml")
 
-batchIndexFinder = Application::BatchIndexFinder.new(feed)
-batch_indexes_queue = batchIndexFinder.builder()
+batch_indexes_queue = Application::BatchIndexFinder.new(feed).builder()
 
 external_service = ExternalService.new
 batch_sender = Application::BatchSender.new(external_service, feed, batch_indexes_queue)
