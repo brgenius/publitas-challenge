f0f4e799cd341b2cc15076fcbca2d5b4fa2f19a6
Bernardo Bucher <bernardobucher@MacBook-Pro-de-Bernardo.local>
2024-12-11 01:18:54 -0300

    adds fallback read from a given url

--- assignment.rb
+++ assignment.rb
@@ -1,9 +1,19 @@
+require 'uri'
 require './requirements'
 
-feed = Infra::Feed.get_from_file("feed.xml")
+feed = nil
+for arg in ARGV
+  feed = Infra::Feed.get_from_url(arg) if arg =~ URI::regexp and arg.end_with? "xml"
+end
 
-batch_indexes_queue = Application::BatchIndexFinder.new(feed).builder()
+def main(feed)
+  batch_indexes_queue = Application::BatchIndexFinder.new(feed).builder()
 
-external_service = ExternalService.new
-batch_sender = Application::BatchSender.new(external_service, feed, batch_indexes_queue)
-batch_sender.send()
+  external_service = ExternalService.new
+  batch_sender = Application::BatchSender.new(external_service, feed, batch_indexes_queue)
+  batch_sender.send()
+end
+
+feed = Infra::Feed.get_from_file("feed.xml") if feed.nil?
+
+main(feed)
--- infra/feed.rb
+++ infra/feed.rb
@@ -1,12 +1,26 @@
 require 'nokogiri'
+require 'open-uri'
+
 require './domain/feed'
 
 module Infra
   class Feed
+    def self.get_from_url(url)
+      puts "Downloading XML file"
+      xml_feed = Nokogiri::XML.parse(URI.open(url))
+      
+      self.builder(xml_feed)
+    end
+
     def self.get_from_file(file_path)
-      # xml_feed = Nokogiri::XML.parse(File.read("feed.xml"))
       xml_feed = Nokogiri::XML.parse(File.read(file_path))
       
+      self.builder(xml_feed)
+    end
+
+    private 
+
+    def self.builder(xml_feed)
       puts "Item count ->"
       item_count = xml_feed.xpath("count(//rss//channel//item)").to_i
       puts item_count
